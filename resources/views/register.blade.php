5f5081d770239c863f3999da311da549d1dfd0f2
AtsuID <atsutrial1@gmail.com>
2025-09-07 21:32:45 +0800

    Adding Simple ui login+register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,20 +6,63 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
 
-  <title>Studify | Login</title>
+  <title>Studify | Register</title>
 </head>
 <body>
-  
-  <section class="login">
 
-    <div class="container mx-auto px-6">
-
+    <section class="register min-h-screen flex items-center justify-center">
+        <div class="container mx-auto bg-white rounded-xl grid grid-cols-2 overflow-hidden max-w-5xl">
+          
+          <div class="images_placeholder">
+            <img src="https://i.pinimg.com/1200x/0f/64/6c/0f646c760cc87a0b880e1fe64092f850.jpg" 
+                 alt="img_register" 
+                 class="w-full h-screen object-cover" />
+          </div>  
+          
+          <div class="forms flex flex-col justify-center px-11 py-10">
+            <h1 class="text-3xl font-bold mb-6">Create Account</h1>
+            
+            <div class="button_cta flex gap-3">
+                <button class="w-full mb-3 bg-[#7be3cf] text-white rounded-full py-3 shadow">
+                <i class='bxl bx-google'></i> Sign up with Google
+                </button>
+                <button class="w-full mb-3 bg-[#ccece6] rounded-full py-2 shadow">
+                <i class='bxl  bx-github'  style='color:#000000'></i> Sign up with Github
+                </button>
+            </div>
+            
+            <div class="forms_input my-4">
+              <input type="text" placeholder="Username" class="w-full rounded-full px-4 py-3 bg-[#ccece6]"/>
+            </div>
+            <div class="forms_input mb-4">
+              <input type="text" placeholder="Password" class="w-full rounded-full px-4 py-3 bg-[#ccece6]"/>
+            </div>
+            <div class="forms_input mb-4 w-full rounded-full px-4 py-3 bg-[#ccece6]">
+                <select id="role" class="w-full rounded-full bg-[#ccece6] focus:outline-none focus:ring-2 focus:ring-[#ccece6]">
+                  <option value="">Silahkan pilih role anda</option>
+                  <option value="admin">Admin</option>
+                  <option value="student">Student</option>
+                </select>
+              </div>
+              
+            
+            <div class="flex items-center mb-4">
+              <input type="checkbox" class="mr-2">
+              <span class="text-sm">I Agree to all terms and Privacy Policy</span>
+            </div>
+            
+            <button class="w-full bg-black text-white rounded-full py-2 shadow">Register</button>
+            
+            <p class="mt-4 text-sm text-gray-600">
+              Sudah Punya Akun? 
+              <a href="#" class="text-green-600">Login</a>
+            </p>
+          </div>
+          
+        </div>
+      </section>
       
 
-    </div>
-
-  </section>
-
 </body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@ Route::get('/test', function () {
     return view('test');
 });
 
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 
 Route::resource('users', UsersController::class);
