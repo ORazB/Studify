9d1bd26f0ca12b8e7c3f7de4388acf6986d714c6
AtsuID <atsutrial1@gmail.com>
2025-09-15 09:18:08 +0800

    adding newest

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,56 @@
 <footer>
+    <script src="https://unpkg.com/boxicons@2.1.3/dist/boxicons.js"></script>
+
     <section>
 
-        <div class="container mx-auto">
+        <div class="container mx-auto grid grid-cols-2">
             
+            <div class="left-content">
+                <img src="#" alt="footer-logo">
+                <h1 class="py-3"> <span class="p-3 bg-black rounded-xl"></span> Studify</h1>
+                <p class="text-[#949393]">Studify – A smart solution for tuition payment
+                    Manage school fees with ease and confidence.</p>
+                
+                <div class="sosmed-cta">
+                    <box-icon name="twitter"></box-icon>
+                    <box-icon name="facebook"></box-icon>
+                    <box-icon name="instagram"></box-icon>
+                    <box-icon name="github"></box-icon>
+                </div>
+                
+            </div>
+
+            <div class="right-content">
+                <div class="cta cursor-pointer">
+                    <h3 class="font-bold">Users</h3>
+                    <p class="text-[#949393]">Students</p>
+                    <p class="text-[#949393]">Admin</p>
+                </div>
+
+                <div class="cta">
+                    <h3 class="font-bold">Menu</h3>
+                    <p class="text-[#949393]">Registrasi</p>
+                    <p class="text-[#949393]">Login</p>
+                </div>
+
+                <div class="cta">
+                    <h3 class="font-bold">Payment</h3>
+                    <p class="text-[#949393]">Pay Now</p>
+                    <p class="text-[#949393]">Check</p>
+                </div>
+            </div>
+
+            <div class="line"></div>    
+        </div>
+
+        <div class="footer-bottom mx-auto flex justify-around">
+            <h1 class="text-[#949393]">@ 2025 Studify. All rights reserved. </h1>
+
+            <div class="settings flex justify-between">
+                <h1 class="underline text-[#949393]">Privacy Policy</h1>
+                <h1 class="underline text-[#949393]">Terms of Service</h1>
+                <h1 class="underline text-[#949393]">Cookies Settings</h1>
+            </div>
         </div>
 
     </section>
